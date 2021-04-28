9f7bbcfce1d2862a9b4f271e9af72f8249e01110
I-Himanshu <83366556+I-Himanshu@users.noreply.github.com>
2021-04-28 23:39:13 +0530

    Create upperLowerTriangle.py

---
+++ upperLowerTriangle.py
@@ -0,0 +1,26 @@
+"""Create A Upper & Lower Triangle as per your requirement"""
+row=input("Enter Number Of Rows >>> ") or 4
+
+row=int(row)
+
+cond=bool(int(input("Enter 1 or 2 for Lower or Upper ∆ >> ")));
+
+#print(cond)
+
+if cond:
+
+  while row>0:
+
+    print("*" * row)
+
+    row=row-1
+
+else:
+
+  start=0;
+
+  while row>=start:
+
+    print("*"*start)
+
+    start=start+1
